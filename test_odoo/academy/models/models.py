fd8bdf87616ee75f242156fdae29d050184219cd
nqvinh04 <nqvinh1995@gmail.com>
2021-01-07 13:14:21 +0700

    update academy

--- test_odoo/academy/models/models.py
+++ test_odoo/academy/models/models.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
 
+class Course(models.Model):
+    _name = 'academy.course'
+    _description = 'Academy Course'
 
-# class academy(models.Model):
-#     _name = 'academy.academy'
-#     _description = 'academy.academy'
+    name = fields.Char(string="Title", required=True, help="Name of the Course")
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class Session(models.Model):
+    _name = 'academy.session'
+    _description = 'Academy Session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     for record in self:
+    #         record.value2 = float(record.value) / 100
